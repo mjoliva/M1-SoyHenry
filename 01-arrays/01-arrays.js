b4d930804dc93c49cd83214e3d7b37e859ffc1f8
Jesu Oliva <olivap.mariajesus@gmail.com>
2022-06-21 09:22:37 -0400

    first commit

---
+++ 01-arrays/01-arrays.js
@@ -0,0 +1,60 @@
+
+function invertirOrden(array) {
+    // Invertir el orden de los elementos del array que recibe por parametro
+    // Que los ultimos elementos, pasen a ser los primeros
+    //
+    // DETALLE: En caso de que el elemento contenga mas de 1 digito, el mismo NO debera ser devuelto
+    // No vale usar el metodo "reverse"
+    
+ 
+};
+
+
+function numeroEnComun(array1, array2) {
+    // Entre los dos array's que recibe la funcion por parametro
+    // Buscar y retornar el valor en comun entre ellos
+   let arr = [];
+   for (let i = 0; i < array1.length; i++){
+    for (let j = 0; j < array2.length; j++){
+        if (array1[i] === array2[j])
+        return array[i];
+        arr.push(Math.min(array1[i], array2[j]))
+    }
+   }
+   return Math.min(...arr);
+};
+
+
+
+function sumaDeArrays(array) {
+    // El array recibido por parametro es un array multidimencional con array's que contienen elementos de tipo number
+    // Tienen que devolver UN SOLO array que solo contenga elementos de tipo number
+    // Sumando los elementos de cada array que contenga dos elementos, y devolviendo la suma del mismo
+    // OJO: Si el elemento dentro del array que ingresa por prop, ya es de tipo number, deben devolverlo como tal dentro del array que retornan.
+};
+
+
+function mismoValorMismosElementos(numero, divisor) {
+    // Tiene que devolver un array con la misma cantidad de elementos que el valor del divisor
+    // Todos los elementos deben tener el mismo valor
+    // OJO: Si el resultado de la division no es un entero, deben devolver false
+
+};
+
+
+function elementoMenorYMayor(array) {
+    // El Array recibido por props es un array que contienen numeros
+    // Tenes que retornar un array
+    // Solamente con el elemento menor y mayor del array recibido
+    
+};
+
+
+
+module.exports = {
+    numeroEnComun,
+    invertirOrden,
+    elementoMenorYMayor,
+    sumaDeArrays,
+    mismoValorMismosElementos
+};
